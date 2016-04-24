46f178a05dc842bbb4acc2f7c823f2a1dc406ce4
jeran <jerantfox@gmail.com>
2016-04-23 17:24:53 -0700

    Implement google maps reverse geocoding.

--- main.js
+++ main.js
@@ -17,7 +17,34 @@ class GoFindMe extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
+      neighborhood: null,
     };
+  }
+
+  getNeighborhood() {
+    fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=37.762219,-122.415614&key=AIzaSyDxT6NnOfiGq5zuVGLDczPVoWwhtwkNWVU', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(function(response) {
+      response.json().then(function(responseObject) {
+        for (var i = 0; i < responseObject.results[0].address_components.length; i++) {
+          var address_component = responseObject.results[0].address_components[i];
+          if (address_component.types.indexOf('neighborhood') !== -1) {
+            this.setState({
+              neighborhood: address_component.long_name,
+            });
+            break;
+          }
+        }
+      }.bind(this)).catch(function(err) {
+        console.error(err);
+      });
+    }.bind(this)).catch(function(err) {
+      console.error(err);
+    });
   }
 
   render() {
@@ -26,7 +53,7 @@ class GoFindMe extends React.Component {
         title="Go Find Me"
         style={styles.container}>
         <Text style={styles.helloWorldText}>
-          Sup Viewers!
+          {this.state.neighborhood || 'loading'}
         </Text>
       </Screen>
     );
@@ -37,6 +64,7 @@ class GoFindMe extends React.Component {
       StatusBarIOS.setStyle('light-content', true);
       StatusBarIOS.setHidden(true);
     }
+    this.getNeighborhood();
   }
 }
 
